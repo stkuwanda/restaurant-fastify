ece5d29a00bb8ba5b000b117a8849ba5f4b5bd96
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-06 23:29:51 +0200

    implemented and registered a local routesGaurdPlugin with onRequest addHook() method and the apropriate app decorator

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,24 +6,23 @@ async function menuHandler(req, res) {
 
 //sync plugin, note usage of next in this synchronous plugin
 export default function recipesPlugin(app, opts, next) {
-	// authentication using a hook
-	// app.addHook('onRequest', async function isChef(req, res) {
-	// 	if (req.headers['x-api-key'] !== 'fastify-rocks') {
-	// 		res.code(401);
-	// 		throw new Error('Invalid API key!');
-	// 	}
-	// });
-
 	// with asynchronous handlers
 	app.route({ method: 'GET', url: '/menu', handler: menuHandler });
 	app.get('/recipes', { handler: menuHandler });
-	app.post('/recipes', async function addToMenu(req, res) {
-		throw new Error('Not implemented!'); // note the direct throw of Errors in the async handlers
-	});
 
-	// with synchronous handlers
-	app.delete('/recipes/:id', function removeFromMenu(req, res) {
-		res.send(new Error('Not implemented!')); // note the imperative use of the res.send(new Error()) method for the synchronous handler
+	// guard all recipes routes with app decorator
+	app.register(async function routesGuardPlugin(plugin, opts) {
+		// authentication using a hook and app decorator
+		plugin.addHook('onRequest', plugin.authOnlyChef);
+
+		plugin.post('/recipes', async function addToMenu(req, res) {
+			throw new Error('Not implemented!'); // note the direct throw of Errors in the async handlers
+		});
+
+		// with synchronous handlers
+		plugin.delete('/recipes/:id', function removeFromMenu(req, res) {
+			res.send(new Error('Not implemented!')); // note the imperative use of the res.send(new Error()) method for the synchronous handler
+		});
 	});
 
 	next();
