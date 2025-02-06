3d8cd98485a0b67c56b2df59bb1c9f6ddee84f27
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-06 11:57:40 +0200

    added initial handling for the POST /recipes route inside the recipesPlugin

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,4 +1,4 @@
-function menuHandler(req, res) {
+async function menuHandler(req, res) {
 	this.log.info('Logging GET /menu from this');
 	req.log.info('Logging GET /menu from req (request object)');
 	throw new Error('Not implemented!');
@@ -7,5 +7,8 @@ function menuHandler(req, res) {
 export default function recipesPlugin(app, opts, next) {
 	app.route({ method: 'GET', url: '/menu', handler: menuHandler });
 	app.get('/recipes', { handler: menuHandler });
+  app.post('/recipes', async function addToMenu(req, res) {
+    throw new Error('Not implemented!');
+  });
 	next();
 }
